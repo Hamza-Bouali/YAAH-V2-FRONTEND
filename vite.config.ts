2f8402abdacc2003373852963bbc2e4830418011
Hamza-Bouali <hamza.bouali@esi.ac.ma>
2025-03-15 12:48:46 +0000

    adjust the requests

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-
 export default defineConfig(({ mode }) => {
-  // Load environment variables based on the current mode (development, production, etc.)
   const env = loadEnv(mode, process.cwd(), '');
+
   console.log('VITE_API_URL:', env.VITE_API_URL);
 
   return {
     plugins: [react()],
     optimizeDeps: {
       exclude: ['lucide-react'],
+    },
+    build: {
+      outDir: 'build', // Change the output directory from 'dist' to 'build'
     },
   };
 });
